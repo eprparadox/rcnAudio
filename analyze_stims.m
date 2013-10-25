a1a742480479767fcbcc2adf202cd8c3c0bb0eaf
eprparadox <jason.vytlacil@gmail.com>
2013-10-24 17:42:50 -0700

    changed the subeject from bm-01 to 99-1

--- analyze_stims.m
+++ analyze_stims.m
@@ -4,7 +4,8 @@
 %%%% includes creating a total power spectrogram for pure and complex tones
 %%%% and creating histograms over the types of all stim
 %sub = 'sequence_test-1';
-sub = 'bm_02-2';
+%sub = 'bm_02-2';
+sub = '99-1'
 load([sub '_rcnAudio_task_map.mat'])
 trial_map = task_map.trial_map;
 
